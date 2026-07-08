c0aaca64ab40edcc3e1568a8df8a1eb74c1f806f
Hao He <haohe90@gmail.com>
2026-07-08 17:04:54 +0800

    Update make_index.sh

--- make_index.sh
+++ make_index.sh
@@ -1,83 +1,90 @@
 #! /bin/bash
-# ubuntu_24.04_LTS
-# last edited at 2025-11-13 by Hao He
+# ubuntu_26.04_LTS
+# last edited at 2026-07-08 by Hao He
 
-#################################  setup  #################################
-conda activate ngs
+# conda activate ngs
 # conda env export --no-builds | grep -v "^prefix: " > ngs.yml
 # conda env create -f ngs.yml
 
-# set directory
-root_dir=/mnt/d/Index/hs/chm13 \
-	&& mkdir -p $root_dir && cd $root_dir \
-	&& echo ">>>>>> start to processing <<<<<<<"
+#################################  setup  #################################
 
+## T2T-CHM13v2.0 (https://github.com/marbl/CHM13), neeed to further processed by scripts before indexing
+# fasta_link=https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz
+# gtf_link=https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/annotation/chm13v2.0_RefSeq_Liftoff_v5.3.gff.gz
 
-## link can be remote download link or local file
-# t2t-chm13
-fasta_link="chm13v2.0.fa.gz"
-gtf_link="chm13v2.0.gtf.gz"
-
-# # hs
+## hs, chromosome: chr1, ..., chrX, chrY, chrM, GLxxx, KIxxx
 # fasta_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_49/GRCh38.primary_assembly.genome.fa.gz
 # gtf_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_49/gencode.v49.basic.annotation.gtf.gz
+# cdna_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_49/gencode.v49.transcripts.fa.gz
 
-# # mm
+## mm, chromosome: chr1, ..., chrX, chrY, chrM, GLxxx, JHxxx, MUxxx
 # fasta_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/GRCm39.primary_assembly.genome.fa.gz
+# cdna_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/gencode.vM38.transcripts.fa.gz
 # gtf_link=https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M38/gencode.vM38.basic.annotation.gtf.gz
 
-# get filename from link
+## fly, chromosome: 2L, 2R, 3L, 3R, 4, X, Y, mitochondrion_genome, ...xxx
+fasta_link=https://ftp.ensembl.org/pub/release-116/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP6.54.dna.toplevel.fa.gz
+gtf_link=https://ftp.ensembl.org/pub/release-116/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.54.63.gtf.gz
+cdna_link=https://ftp.ensembl.org/pub/release-116/fasta/drosophila_melanogaster/cdna/Drosophila_melanogaster.BDGP6.54.cdna.all.fa.gz
+
+## cel, chromosome: I, II, III, IV, V, X, MtDNA
+# fasta_link=https://ftp.ensembl.org/pub/release-116/fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz
+# gtf_link=https://ftp.ensembl.org/pub/release-116/gtf/caenorhabditis_elegans/Caenorhabditis_elegans.WBcel235.63.gtf.gz
+# cdna_link=https://ftp.ensembl.org/pub/release-116/fasta/caenorhabditis_elegans/cdna/Caenorhabditis_elegans.WBcel235.cdna.all.fa.gz
+
+
+## set directory
+# target_dir=/mnt/d/Index/hs/v49
+target_dir=/mnt/d/Index/fly
+root_dir=$target_dir && mkdir -p $root_dir && cd $root_dir && echo ">>>>>> start to processing <<<<<<<"1
+
+
+#################################  process data  #################################
+# fasta
 fasta_gz=$(basename "$fasta_link")
-gtf_gz=$(basename "$gtf_link")
-
-
-#################################  download and process data  #################################
-# bgzip -@ 4 -k $fasta_gz
-# file $fasta_gz
-
-## fasta
+fasta=${fasta_gz%.gz}
 [ ! -f "$fasta_gz" ] && echo "downloading fasta" && wget $fasta_link -O $fasta_gz
-zcat "$fasta_gz" | bgzip -@ 4 -c > "${fasta_gz}.tmp" && mv "${fasta_gz}.tmp" "$fasta_gz"
-[ ! -f "${fasta_gz}.fai" ] && echo "index fasta" && samtools faidx -@ 4 $fasta_gz
+[ ! -f "${fasta}.fai" ] && gunzip -k $fasta_gz && samtools faidx $fasta
 
-## gtf
+# gtf
+gtf_gz=$(basename "$gtf_link")
+gtf=${gtf_gz%.gz}
 [ ! -f "$gtf_gz" ] && echo "downloading gtf" && wget $gtf_link -O $gtf_gz
-zcat "$gtf_gz" | bgzip -@ 4 -c > "${gtf_gz}.tmp" && mv "${gtf_gz}.tmp" "$gtf_gz"
+[ ! -f "$gtf" ] && gunzip -k $gtf_gz
 
-# get ungzipped files
-[[ ! -s "${fasta_gz%.gz}" ]] && gzip -d -k $fasta_gz && samtools faidx -@ 4 ${fasta_gz%.gz}
-[[ ! -s "${gtf_gz%.gz}" ]] && gzip -d -k $gtf_gz
+# cdna
+if [ -z "${cdna_link+x}" ]; then
+    echo "cdna_link does not exist, extract cdna first"
+    cdna_gz=cdna.fa.gz
+	gffread -w - -g $fasta_gz $gtf_gz | gzip > "$cdna_gz"
+else
+    cdna_gz=$(basename "$cdna_link")
+	[ ! -f "$cdna_gz" ] && echo "downloading cdna" && wget $cdna_link -O $cdna_gz
+fi
+
 
 
 #################################  salmon index  #################################
 # salmon use ultra fast quasi mapping on transcript instead of genome
-mkdir -p salmon
+# mkdir -p salmon
 # generate decoy
-zgrep "^>" $fasta_gz | cut -d " " -f 1 > decoys.txt && sed -i -e 's/>//g' decoys.txt
-# generate cdna, generate don't support .gz files
-zcat "$gtf_gz" | gffread - -g ${fasta_gz%.gz} -w cdna.fa
-
+# zgrep "^>" $fasta_gz | cut -d " " -f 1 > decoys.txt && sed -i -e 's/>//g' decoys.txt
 # add custome genome, usually like FPs, Cas9, resistance genes
-custome_fa="../../chrN.fa"
-custome_gtf="../../chrN.gtf"
-if [[ -f "$custome_fa" && -f "$custome_gtf" ]]; then
-	echo "add custome genome ...."
-	gffread -g $custome_fa $custome_gtf -w custom_cdna.fa
-	grep "^>" $custome_fa | cut -d " " -f 1 > custom_decoys.txt && \
-		sed -i -e 's/>//g' custom_decoys.txt && \
-		cat custom_decoys.txt >> decoys.txt
+# if [[ -f "custom_genome.fa" && -f "custom_genome.gtf" ]]; then
+	# echo "add custome genome ...."
+	# gffread -w custom_cdna.fa -g custom_genome.fa custom_genome.gtf && gzip custom_cdna.fa
+	# gzip -k custom_genome.fa && \
+		# zgrep "^>" custom_genome.fa.gz | cut -d " " -f 1 > custom_decoys.txt && \
+		# sed -i -e 's/>//g' custom_decoys.txt && cat custom_decoys.txt >> decoys.txt
 	# generate gentrome
-	cat custom_cdna.fa cdna.fa $custome_fa ${fasta_gz%.gz} | \
-		bgzip -@ 4 -c > gentrome.fa.gz
-else
-	cat cdna.fa ${fasta_gz%.gz} | bgzip -@ 4 -c > gentrome.fa.gz
-fi
-
+	# zcat custom_cdna.fa.gz $cdna_gz custom_genome.fa.gz $fasta_gz > gentrome.fa.gz
+# else
+	# zcat $cdna_gz $fasta_gz > gentrome.fa.gz
+# fi
 # index
-salmon index -p 8 -t gentrome.fa.gz -d decoys.txt -i salmon --gencode
+# salmon index -p 8 -t gentrome.fa.gz -d decoys.txt -i salmon --gencode
 # remove unused files
-if [ $? -eq 0 ]; then rm ${custome_fa}.fai custom_decoys.txt decoys.txt custom_cdna.fa cdna.fa gentrome.fa.gz; fi
-
+# if [ $? -eq 0 ]; then rm custom_decoys.txt custom_genome.fa.gz custom_cdna.fa.gz gentrome.fa.gz custom_genome.fa.fai; fi
 
 
 #################################  bowtie2 index  #################################
@@ -88,10 +95,7 @@ bowtie2-build --threads 8 $fasta_gz bowtie2/bowtie2
 
 #################################  star index  #################################
 # star is specifically designed for RNA mapping on genome
-# star don't support compressed .fa and .gtf files
-mkdir -p star
-STAR --runThreadN 8 --runMode genomeGenerate --genomeDir star \
-	--genomeFastaFiles "${fasta_gz%.gz}" --sjdbGTFfile "${gtf_gz%.gz}" \
-	--sjdbOverhang 149 --limitGenomeGenerateRAM 24000000000 # ~ x/1024/1000/1000 GB
-# remove unused files
-if [ $? -eq 0 ]; then rm ${fasta_gz%.gz} ${fasta_gz%.gz}.fai ${gtf_gz%.gz}; fi
+# mkdir -p star
+# STAR --runThreadN 8 --runMode genomeGenerate --genomeDir star \
+	# --genomeFastaFiles $fasta --sjdbGTFfile $gtf \
+	# --sjdbOverhang 149 --limitGenomeGenerateRAM 30000000000 # ~ x/1024/1000/1000 GB
